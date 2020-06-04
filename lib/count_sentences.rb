d9d61860302da99f13c75d4f57d2c41fda9994e1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-04 18:30:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
       TRUE
     else
       FALSE
+    end
   end
 
   def exclamation?
@@ -22,6 +23,7 @@ class String
       TRUE
     else
       FALSE
+    end 
   end
 
   def count_sentences
