9bb06cb4fb200f1a7662fa2cbc1214d030cea13c
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-04 18:48:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,8 @@ class String
   end
 
   def count_sentences
-    self.gsub!(/.?!/, "*")
-    self.split("*")
+    converted = self.gsub!(/.?!/, "*")
+    string = converted.split("*")
     self.delete("*")
     self.count
     # periods = self.count(".")
