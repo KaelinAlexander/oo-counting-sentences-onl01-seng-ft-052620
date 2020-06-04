b750f958a6aa77d42067b1c9288c3145baf929ea
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-04 18:42:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,8 @@ class String
   end
 
   def count_sentences
-    self.gsub(/.?!/, "*")
+    self.gsub!(/.?!/, "*")
+    self.split("*")
     # periods = self.count(".")
     # questions = self.count("?")
     # exclamations = self.count("!")
